de4e437da06217b0d5f5eade9f5adacff2735eae
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2023-11-18 21:52:29 +0300

    feat: Реализована навигация по табам внизу экрана

---
+++ src/screens/CartScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const CartScreen = () => {
+  return (
+    <View>
+      <Text>CartScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
---
+++ src/screens/PaymentScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const PaymentScreen = () => {
+  return (
+    <View>
+      <Text>PaymentScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
